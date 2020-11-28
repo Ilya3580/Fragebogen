dd989a95db28750cc6c541886433d42ea466ec2e
Ilya3580 <ilya.smetanin.2002@mail.ru>
2020-11-28 09:30:40 +0300

    3

--- app/src/main/java/com/example/fragebogen/Adapter.kt
+++ app/src/main/java/com/example/fragebogen/Adapter.kt
@@ -1,8 +1,6 @@
 package com.example.fragebogen
 
-import android.app.PendingIntent.getActivity
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.net.ConnectivityManager
@@ -10,14 +8,15 @@ import android.os.Build
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -40,12 +39,20 @@ class Adapter(items: ArrayList<ObjectQuestions>, context: Context)
         objectQuestions = getItem(position)!!
         if(objectQuestions.bitmapFlag)
         {
-            view = LayoutInflater.from(context).inflate(R.layout.fragment_listview_image, parent,false)
+            view = LayoutInflater.from(context).inflate(
+                R.layout.fragment_listview_image,
+                parent,
+                false
+            )
             progressBar = view.findViewById(R.id.progressBar)
             generateImageView(position)
             generateTextView(position)
         }else if(objectQuestions.editTextFlag){
-            view = LayoutInflater.from(context).inflate(R.layout.fragment_listview_edit_text, parent,false)
+            view = LayoutInflater.from(context).inflate(
+                R.layout.fragment_listview_edit_text,
+                parent,
+                false
+            )
             generateEditText(position)
         }else{
             view = LayoutInflater.from(context).inflate(R.layout.fragment_listview, parent, false)
@@ -53,9 +60,15 @@ class Adapter(items: ArrayList<ObjectQuestions>, context: Context)
             if(position !=0) {
                 if (position == count - 1) {
                     val textView = view.findViewById<TextView>(R.id.textView)
-                    textView.background = ContextCompat.getDrawable(context, R.drawable.round_corner_orange)
+                    textView.background = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.round_corner_orange
+                    )
                 } else {
-                    view.background = ContextCompat.getDrawable(context, getItem(position)!!.idBackground)
+                    view.background = ContextCompat.getDrawable(
+                        context,
+                        getItem(position)!!.idBackground
+                    )
                 }
             }
         }
@@ -73,6 +86,16 @@ class Adapter(items: ArrayList<ObjectQuestions>, context: Context)
         var editText = view.findViewById<EditText>(R.id.editText)
         editText.background = ContextCompat.getDrawable(context, getItem(position)!!.idBackground)
         editText.setText(editTextText)
+        editText.setOnKeyListener { view, i, keyEvent ->
+            var consumed = false
+            if (i == KEYCODE_ENTER) {
+                val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view?.windowToken, 0)
+                editText.setText(editText.text.toString().replace("\n", ""))
+                consumed = true //это если не хотим, чтобы нажатая кнопка обрабатывалась дальше видом, иначе нужно оставить false
+            }
+            return@setOnKeyListener consumed
+        }
         editText.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
             }
